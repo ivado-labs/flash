d8f0377e6db140c7a25b03f647b4786c13c5c2f7
manalboucenna <manal.boucenna@brp.com>
2024-12-12 10:55:20 -0500

    feat: Add ydata-profilling tutorials

--- notebooks/ML/Nixtla/utils.py
+++ notebooks/ML/Nixtla/utils.py
@@ -1,20 +1,13 @@
 import pandas as pd
 from utilsforecast.evaluation import evaluate
-from utilsforecast.losses import coverage, mse
+from utilsforecast.losses import coverage, mape, mse
 
 
 def evaluate_cross_validation(df: pd.DataFrame, level: list[int]) -> pd.DataFrame:
     models = df.loc[:, ~df.columns.str.contains("unique_id|y|ds|cutoff|lo|hi")].columns
-
-    evals = []
-    for cutoff in df["cutoff"].unique():
-        cutoff_df = df[df["cutoff"] == cutoff].drop(columns="cutoff")
-        eval_ = evaluate(df=cutoff_df, metrics=[mse, coverage], models=models, level=level)
-        evals.append(eval_)
-
-    evals = pd.concat(evals)
+    evals = evaluate(df=df, metrics=[mape, mse, coverage], models=models, level=level)
     evals["best_model"] = evals.apply(
-        lambda row: row[models].idxmin() if (row["metric"] == "mse") else row[models].idxmax(), axis=1
+        lambda row: row[models].idxmax() if (row["metric"] == "coverage") else row[models].idxmin(), axis=1
     )
     return evals
 
